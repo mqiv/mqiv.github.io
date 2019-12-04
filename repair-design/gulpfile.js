78a15064b7ac55877a94590bd110f5237218b867
mqivo6ll <mqivo6ll@gmail.com>
2019-12-04 21:04:07 +0300

    reset .gitignore, feat: add gulp-sass in gulpfile.js

--- repair-design/gulpfile.js
+++ repair-design/gulpfile.js
@@ -1,22 +1,26 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
 // Static server
-gulp.task('browser-sync', function() {
-  browserSync.init({
-      server: {
-          baseDir: "./"
-      }
-  });
-  gulp.watch("./css/*.css", ['sass']);
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
- 
-gulp.task('min.css', function () {
-    gulp.src('./css/*.css')
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('css'));
-});
+function bs() {
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+exports.serve = bs;
