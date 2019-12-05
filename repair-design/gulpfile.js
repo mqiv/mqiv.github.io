14b66076f595290271f4340dda5e41026bf90e2d
mqivo6ll <mqivo6ll@gmail.com>
2019-12-05 23:48:51 +0300

    feat: add Futura Book & Futura Demic

--- repair-design/gulpfile.js
+++ repair-design/gulpfile.js
@@ -18,7 +18,7 @@ function bs() {
 };
 
 function serveSass() {
-    return src("./sass/*.sass")
+    return src("./sass/**/*.sass")
         .pipe(sass())
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
